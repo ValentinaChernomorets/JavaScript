9e58c270297868e4e0be55a21573817eb0cf6ba4
Valentina Chernomorets <vcapustina8@gmail.com>
2023-10-16 08:57:24 +0300

    Lessons 8

---
+++ LESSONS_8/part-2/for-counter.js
@@ -0,0 +1,20 @@
+for (let n = 1; n <= 9; n++) {
+    console.log(n)
+}
+
+let nSecond = 1
+for (;nSecond <= 9;) {
+    console.log(nSecond)
+    nSecond++
+}
+
+let nThird = 1
+while (nThird <= 9) {
+
+}
+
+// HW1:
+// - use while remake the code for a count down: 9 8 ....1
+for (let nFive = 9; nFive > 0; nFive--) {
+    console.log(nFive)
+}
