35ce9273a867170fc8f4dc7f8d467c4f254c6f1d
Valentina Chernomorets <vcapustina8@gmail.com>
2023-11-23 18:45:18 +0200

    LESSONS 15 HW

---
+++ LESSONS_15/game.js
@@ -0,0 +1,20 @@
+let players = [
+    { // 0
+        name: "player777",
+        rating: 4.5,
+        xp: 1_000_000,
+        skills: ["fast", "exact", "silent"],
+    },
+    { // 1
+        name: "Marry",
+        rating: 5.0,
+        xp: 2_000_000,
+        skills: ["silent", "mvp"],
+    },
+    { // 1
+        name: "Peters0n",
+        rating: 2.5,
+        xp: 1_000,
+        skills: [],
+    },
+]
---
+++ LESSONS_15/objects.js
@@ -0,0 +1,42 @@
+// HW1:
+// choose a book, film, product...
+// try to structure the information about them using literal object
+
+let music = {
+    style: "jazz",
+    year: 1917,
+    performer: "Louis Armstrong",
+    albums: ["Ella and Louis", "The Great Summit", "The Great Summit"],
+    award: ["Grammy Award for Best Vocal Performance", "Grammy Lifetime Achievement Award", "Independent Music Award for Best Album"],
+    movie: ["The Five Pennies", "Paris Blues", "Louis Armstrong's Black & Blues", "A Man Called Adam", "The Glenn Miller Story"],
+    book: {
+        first: "What a Wonderful",
+        second: "Swing the music",
+        third: "Louis Armstrong, in His Own Words",
+        fourth: "Louis Armstrong, a jazzmaster",
+    }
+}
+
+let retrowave = {
+    style: "synthwave",
+    stylistic: "electronic",
+    year: 1980,
+    origins: ["soundtracks", "electro", "Eurodisco", "French house", "video game music", "chiptune"],
+    albums: ["OutRun", "Reborn", "Galactic Melt", "RR7349", "Endless Summer", "Persuasion System"],
+    movie: ["Kung Fury", "The Rise of the Synths", "Turbo Kid", "A Man Called Adam", "The Glenn Miller Story", " Blade Runner", "Drive", "Stranger Things"],
+    game: [ "Furi", "Hotline Miami 1 & 2", "Nex Machina", "Streets Of Rage 2"]
+}
+
+let LordRings = {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    releaseYear: 2001,
+    director: "Peter Jackson",
+    characters: {
+        hobbits: ["Frodo Baggins", "Samwise Gamgee", "Merry Brandybuck", "Pippin Took"],
+        wizards: ["Gandalf"],
+        elves: ["Legolas", "Galadriel", "Elrond"],
+        humans: ["Aragorn", "Boromir", "Faramir"],
+        dwarves: ["Gimli"]
+      },
+      awards: ["11 Academy Awards", "4 BAFTA Awards"]
+}
